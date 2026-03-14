3260ae28a966291937dd4e30dc4aa1ae854a62c6
Yevgeni Shapiro <23049337+app1980max@users.noreply.github.com>
2026-03-14 23:40:05 +0200

    update

--- modules/vclusters/variable.tf
+++ modules/vclusters/variable.tf
@@ -1,2 +1,27 @@
+
 variable "name" {}
 variable "namespace" {}
+
+variable "loft_host" {
+  description = "Number of additional control planes for HA"
+  type        = string
+  default     = "vcluster-dev.appflex.io"
+}
+
+variable "loft_user" {
+  description = "Number of additional control planes for HA"
+  type        = string
+  default     = "admin"
+}
+
+variable "loft_password" {
+  description = "Number of additional control planes for HA"
+  type        = string
+  default     = "admin"
+}
+
+variable "kubeconfig_path" {
+  description = "Number of additional control planes for HA"
+  type        = string
+  default     = "~/.kube/config"
+}
